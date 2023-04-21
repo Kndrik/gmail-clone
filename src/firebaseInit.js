27ea5553b94ba9d88ca5634df5e3389f70397bd7
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-21 10:58:21 +0900

    Add firebase

---
+++ src/firebaseInit.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAS9tVRa2z2KPHQxGDUW6qJ-BJLClztnzA",
+    authDomain: "clone-94a85.firebaseapp.com",
+    projectId: "clone-94a85",
+    storageBucket: "clone-94a85.appspot.com",
+    messagingSenderId: "513730988433",
+    appId: "1:513730988433:web:9f89b5abb176eeaf123230"
+  };
+
+  const app = initializeApp(firebaseConfig);
