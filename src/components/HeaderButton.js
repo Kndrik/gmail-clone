5afa3f5b124be09c3246c27f12b1f5c7682557d1
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-21 15:50:43 +0900

    Add click events to search and delete buttons

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,7 +1,13 @@
 const HeaderButton = (props) => {
     const classes = "headerButton " + props.classes + (props.light ? '' : ' dark');
     return (
-        <button className={classes}>
+        <button 
+            type={props.type} 
+            className={classes} 
+            onClick={props.clickEvent ?
+                        props.clickEvent : 
+                        (e) => {e.preventDefault()}}
+        >
             <svg className="headerIcon" xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 96 960 960" width="25">
                 <path 
                     fill={props.light ? "#606367" : "#454746"} 
