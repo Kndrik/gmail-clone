5ee411c89609ac37ac71eda6ec43b5d29a55f397
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-22 00:42:42 +0900

    Change scrollbar to overlay

--- src/components/NavElement.js
+++ src/components/NavElement.js
@@ -4,7 +4,7 @@ const NavElement = (props) => {
     return (
         <li className="navElement">
             <NavLink className="navLink" to={props.pathTo}>
-                <svg className="navIcon" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+                <svg className="navIcon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 96 960 960" width="18">
                     <path 
                         fill="#606367" 
                         d={props.svgData}
