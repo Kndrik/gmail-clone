6781cc7a6719b3c945bec20d2c9672dc46521b67
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-21 16:22:10 +0900

    Add new message button visuals and sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+const Sidebar = (props) => {
+    return (
+        <div className="sidebar">
+            <div className="sidebarTop">
+                <div className="newMessageContainer">
+                    <button className="newMessageButton">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 96 960 960" width="25"><path d="M187 877h32l435-437-32-32-435 437v32Zm610-479L665 266l37.823-38.823Q723 207 752 207t52 21l33 32q23 22 20.5 51t-22.463 48.963L797 398ZM162.327 948q-19.889 0-33.608-13.714Q115 920.571 115 900.689v-65.627q0-9.062 3.5-17.562Q122 809 129 802l494-494 133 131-494 495q-7 7-16.217 10.5Q236.565 948 228 948h-65.673ZM639 424l-17-16 32 32-15-16Z"/></svg>
+                        <span>Nouveau message</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
