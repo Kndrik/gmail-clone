5944bd30fe4f07f5af6daeb41f3e31fce6ed3dcf
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-21 14:30:20 +0900

    Fix gmail header logo's height

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = (props) => {
         <div className="header">
             <div className="leftContainer">
                 <div className="menuSvgContainer">
-                    <svg className="menuIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path fill="#606367" d="M150 816q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150 756h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810 816H150Zm0-210q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150 546h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810 606H150Zm0-210q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150 336h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810 396H150Z"/></svg>
+                    <svg className="menuIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path fill="#606367" d="M110.391 831.827v-79.218h739.218v79.218H110.391Zm0-216.218v-79.218h739.218v79.218H110.391Zm0-216.218v-79.783h739.218v79.783H110.391Z"/></svg>
                 </div>
                 <img className="gmailLogo" src={logo} alt="gmail logo" />
             </div>
