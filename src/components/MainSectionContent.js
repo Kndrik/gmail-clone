4721f5babce6664aae5cc23d3d5b8e83924d938a
Charles Lamarque <charles.lamarque69@gmail.com>
2023-04-24 16:43:06 +0900

    Add content footer

--- src/components/MainSectionContent.js
+++ src/components/MainSectionContent.js
@@ -9,6 +9,19 @@ const MainSectionContent = (props) => {
         <div onScroll={props.handleScroll} className="mainSectionContent">
             <MainSectionContentSortBar />
             <EmailList />
+            <div className="footer">
+                <div className="left">
+                    <div>9,13 Go utilisés sur 15 Go</div>
+                </div>
+                <div className="middle">
+                    <div>
+                        Conditions d'utilisation - Confidentialité - Règlement du programme
+                    </div>
+                </div>
+                <div className="right">
+                    <div>Dernière activité sur le compte : il y a 10 minutes.</div>
+                </div>
+            </div>
         </div>
     )
 };
